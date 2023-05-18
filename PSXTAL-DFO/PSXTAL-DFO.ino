a9ca44ad671de051c54b75fbc5a5890f7fa755c4
VajskiDs <87033268+L10N37@users.noreply.github.com>
2023-05-18 18:43:35 +1000

    Update PSXTAL-DFO.ino

--- PSXTAL-DFO/PSXTAL-DFO.ino
+++ PSXTAL-DFO/PSXTAL-DFO.ino
@@ -1,13 +1,21 @@
+#include <Wire.h>
 #include <Adafruit_SI5351.h>
 #define sensepin 2
 Adafruit_SI5351 clockgen = Adafruit_SI5351();
-
+bool NTSC = false;
 
 void setup(){
+
+Wire.begin();             // initialize I2C interface
+Wire.setClock(400000);    // set I2C clock speed to 400 kHz
 clockgen.begin();
 clockgen.enableOutputs(true);
-DDRD = 0xFF;    // clear every pin on PD and pull them all low as outputs temporarily  (The Port D stuff is especially important for videomode sensing to work)
-PORTD = 0x00;   // any pins requiring different states are set up just after this
+DDRD = 0xFF;    // Set known port state //
+PORTD = 0x00;   //                      //
+DDRB = 0xFF;  
+PORTB = 0x00;
+DDRC = 0xFF;
+PORTC = 0x00;
 pinMode (sensepin, INPUT);
 }
 
@@ -15,21 +23,26 @@ pinMode (sensepin, INPUT);
 void loop(){
 
 
-if (PIND == 0x4) {
+if (PIND == 0x4 && NTSC == false) {
 //NTSC
   clockgen.setupPLL(SI5351_PLL_A, 30, 30, 450);
   clockgen.setupMultisynth(0, SI5351_PLL_A, 13, 5, 5);
-  while (PIND== 0x4); //lock up here and do nothing 
+  NTSC = true;
     }
 
-if (PIND != 0x4) {
+if (PIND == 0x00 && NTSC == true) {
 //PAL
   clockgen.setupPLLInt(SI5351_PLL_A, 16);
   clockgen.setupMultisynth(0, SI5351_PLL_A, 7, 14, 27);
-  while (PIND!= 0x4); //lock up here and do nothing 
+  NTSC = false;
     }
-
 
 }
 
-//not tested.
+/*
+Use 8Mhz internal osc
+
+LFUSE = 0xC2
+HFUSE = 0xF9
+EFUSE = 0xFF
+*/
