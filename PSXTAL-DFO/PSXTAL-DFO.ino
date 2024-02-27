a8c6b14aab44f57fc904f1b8272ae8787a59347b
VajskiDs <87033268+L10N37@users.noreply.github.com>
2024-02-27 18:04:52 +1100

    Update PSXTAL-DFO.ino

--- PSXTAL-DFO/PSXTAL-DFO.ino
+++ PSXTAL-DFO/PSXTAL-DFO.ino
@@ -1,6 +1,7 @@
 #include <Wire.h>
 #include <Adafruit_SI5351.h>
 #define sensepin 2
+#define senseHigh bitRead(PIND, 2) == HIGH
 Adafruit_SI5351 clockgen = Adafruit_SI5351();
 bool NTSC = false;
 
@@ -10,27 +11,19 @@ Wire.begin();             // initialize I2C interface
 Wire.setClock(400000);    // set I2C clock speed to 400 kHz
 clockgen.begin();
 clockgen.enableOutputs(true);
-DDRD = 0xFF;    // Set known port state //
-PORTD = 0x00;   //                      //
-DDRB = 0xFF;  
-PORTB = 0x00;
-DDRC = 0xFF;
-PORTC = 0x00;
-pinMode (sensepin, INPUT);
+bitClear (DDRD, 2);
 }
-
 
 void loop(){
 
-
-if (PIND == 0x4 && NTSC == false) {
+if (senseHigh && NTSC == false) {
 //NTSC
   clockgen.setupPLL(SI5351_PLL_A, 30, 30, 450);
   clockgen.setupMultisynth(0, SI5351_PLL_A, 13, 5, 5);
   NTSC = true;
     }
 
-if (PIND == 0x00 && NTSC == true) {
+if (!senseHigh && NTSC == true) {
 //PAL
   clockgen.setupPLLInt(SI5351_PLL_A, 16);
   clockgen.setupMultisynth(0, SI5351_PLL_A, 7, 14, 27);
